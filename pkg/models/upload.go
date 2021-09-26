c2b9b0af9d1827bfb67e0027122414e1844dbce2
Matan Budimir <matanbudimir@gmail.com>
2021-09-26 23:13:58 +0200

    Gofmt

--- pkg/models/upload.go
+++ pkg/models/upload.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Upload struct {
-	ID        uint      `json:"id" gorm:"primaryKey;type:int(11);unsigned;column:id"`
-	Name      string    `json:"name" gorm:"type:varchar(255);column:name"`
-	Path      string    `json:"-" gorm:"type:varchar(500);column:path"`
-	Type      string    `json:"type" gorm:"type:varchar(1);column:type"`
-	Public    bool      `json:"-" gorm:"type:tinyint(1);column:public"`
-	DownloadURL string `json:"download_url"`
-	CreatedAt time.Time `json:"-" gorm:"type:timestamp;column:created_at"`
-	UpdatedAt time.Time `json:"-" gorm:"type:timestamp;column:updated_at"`
+	ID          uint      `json:"id" gorm:"primaryKey;type:int(11);unsigned;column:id"`
+	Name        string    `json:"name" gorm:"type:varchar(255);column:name"`
+	Path        string    `json:"-" gorm:"type:varchar(500);column:path"`
+	Type        string    `json:"type" gorm:"type:varchar(1);column:type"`
+	Public      bool      `json:"-" gorm:"type:tinyint(1);column:public"`
+	DownloadURL string    `json:"download_url"`
+	CreatedAt   time.Time `json:"-" gorm:"type:timestamp;column:created_at"`
+	UpdatedAt   time.Time `json:"-" gorm:"type:timestamp;column:updated_at"`
 }
 
 func (upload *Upload) SetDownloadURL() {
