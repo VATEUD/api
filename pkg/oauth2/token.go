356c67bcb2ba9e120307b63407db9b9316da6210
Matan Budimir <matanbudimir@gmail.com>
2021-10-14 21:07:11 +0200

    Add headers

--- pkg/oauth2/token.go
+++ pkg/oauth2/token.go
@@ -7,6 +7,9 @@ import (
 )
 
 func Token(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Cache-Control", "no-store")
+	w.Header().Set("Pragma", "no-cache")
 	req, err := newAccessTokenRequest(r)
 
 	if err != nil {
