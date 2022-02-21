14fac3096970840de03a9e8aebbb7648f6f9db43
Matan Budimir <matanbudimir@gmail.com>
2022-02-21 12:29:04 +0100

    Fixes

--- pkg/vatsim/webhook/member.go
+++ pkg/vatsim/webhook/member.go
@@ -4,6 +4,8 @@ import (
 	"api/internal/pkg/database"
 	"api/internal/pkg/logger"
 	"api/pkg/models"
+	"errors"
+	"gorm.io/gorm"
 	"net/http"
 )
 
@@ -26,10 +28,32 @@ func (member Member) Update(result chan Result) {
 	var user models.User
 
 	if err := database.DB.Where("id = ?", member.Id).First(&user).Error; err != nil {
-		logger.Log.Println("User not found")
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			logger.Log.Println("User not found")
+			result <- Result{
+				StatusCode: http.StatusOK,
+				Message:    "Member updated",
+			}
+			return
+		}
+
+		logger.Log.Errorln("Error occurred while executing the query. Error:", err.Error())
 		result <- Result{
-			StatusCode: http.StatusOK,
-			Message:    "Member updated",
+			StatusCode: http.StatusInternalServerError,
+			Message:    "Internal Server Error",
+		}
+		return
+	}
+
+	var subdivision models.Subdivision
+
+	subError := database.DB.Where("code = ?", member.SubdivisionId).First(&subdivision).Error
+
+	if subError != nil && !errors.Is(subError, gorm.ErrRecordNotFound) {
+		logger.Log.Errorln("Error occurred while processing the query. Error:", subError.Error())
+		result <- Result{
+			StatusCode: http.StatusInternalServerError,
+			Message:    "Internal Server Error",
 		}
 		return
 	}
@@ -39,8 +63,11 @@ func (member Member) Update(result chan Result) {
 	user.Rating = member.Rating
 	user.PilotRating = member.PilotRating
 	user.RegionID = member.RegionId
+	user.RegionName = Regions[member.RegionId]
 	user.DivisionID = member.DivisionId
-	user.SubdivisionID = member.SubdivisionId
+	user.DivisionName = Divisions[member.DivisionId]
+	user.SubdivisionID = subdivision.Code
+	user.SubdivisionName = subdivision.Name
 
 	if err := database.DB.Updates(&user).Error; err != nil {
 		logger.Log.Errorln("Failed to update member's data. Error:", err.Error())
@@ -49,6 +76,19 @@ func (member Member) Update(result chan Result) {
 			Message:    "Failed to update member's data",
 		}
 		return
+	}
+
+	// TODO - investigate why subdivision doesn't get saved if it's set to null
+	if errors.Is(subError, gorm.ErrRecordNotFound) {
+		if err := database.DB.Exec("UPDATE users SET subdivision_id = NULL, subdivision_name = NULL WHERE id = ?", user.ID).Error; err != nil {
+			logger.Log.Errorln("Failed to update member's data. Error:", err.Error())
+			result <- Result{
+				StatusCode: http.StatusInternalServerError,
+				Message:    "Failed to update member's data",
+			}
+			return
+		}
+
 	}
 
 	logger.Log.Println("Received member's data -", member)
---
+++ pkg/vatsim/webhook/types.go
@@ -0,0 +1,31 @@
+package webhook
+
+var Divisions = map[string]string {
+	"BRZ": "Brazil (VATBRZ)",
+	"CAM": "Central America",
+	"CAN": "Canada",
+	"CAR": "Caribbean",
+	"EUD": "Europe (except UK)",
+	"GBR": "United Kingdom",
+	"IL": "Israel (VATIL)",
+	"JPN": "Japan",
+	"KOR": "Korea",
+	"MCO": "Mexico",
+	"MENA": "Middle East and North Africa",
+	"NZ": "New Zealand (VATNZ)",
+	"PAC": "Australia (VATPAC)",
+	"PRC": "People's Republic of China",
+	"ROC": "Republic of China (Taiwan)",
+	"RUS": "Russia",
+	"SAM": "South America",
+	"SEA": "Southeast Asia (VATSEA)",
+	"SSA": "Sub-Sahara Africa",
+	"USA": "United States",
+	"WA": "West Asia",
+}
+
+var Regions = map[string]string {
+	"AMAS": "Americas",
+	"APAC": "Asia Pacific",
+	"EMEA": "Europe, Middle East and Africa",
+}
