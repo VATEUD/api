4b52f3f951cef6317e6d893375c29270d22d5351
Matan Budimir <matanbudimir@gmail.com>
2022-02-23 11:22:38 +0100

    Change the default value to false

--- pkg/models/subdivision.go
+++ pkg/models/subdivision.go
@@ -8,7 +8,7 @@ type Subdivision struct {
 	Name             string                  `json:"name" gorm:"type:varchar(255);column:name"`
 	WebsiteURL       string                  `json:"website_url" gorm:"type:varchar(255);column:website_url"`
 	DiscordServerURL *string                 `json:"discord_server_url" gorm:"type:varchar(100);column:discord_server_url"`
-	WithinVATEUD     bool                    `json:"-" gorm:"type:tinyint(1);column:within_eud;default:false"`
+	WithinVATEUD     bool                    `json:"-" gorm:"type:tinyint(1);column:within_eud;default:true"`
 	Instructors      []SubdivisionInstructor `json:"instructors,omitempty" gorm:"foreignKey:SubdivisionID"`
 	CreatedAt        time.Time               `json:"-" gorm:"type:timestamp;column:created_at"`
 	UpdatedAt        time.Time               `json:"-" gorm:"type:timestamp;column:updated_at"`
